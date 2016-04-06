670f2787921cd4dd058d6d03765fc80ddd607382
Josh Albrecht <joshmalbrecht@gmail.com>
2016-04-06 14:21:53 -0500

    Challenge 261 Init Commit

---
+++ Challenge_261/MagicSquares.java
@@ -0,0 +1,22 @@
+/*
+Description:
+A 3x3 magic square is a 3x3 grid of the numbers 1-9 such that each row, column, and major diagonal adds up to 15. Here's an example:
+8 1 6
+3 5 7
+4 9 2
+The major diagonals in this example are 8 + 5 + 2 and 6 + 5 + 4. (Magic squares have appeared here on r/dailyprogrammer before, in 
+#65 [Difficult] in 2012.)
+Write a function that, given a grid containing the numbers 1-9, determines whether it's a magic square. Use whatever format you want 
+for the grid, such as a 2-dimensional array, or a 1-dimensional array of length 9, or a function that takes 9 arguments. You do not 
+need to parse the grid from the program's input, but you can if you want to. You don't need to check that each of the 9 numbers appears 
+in the grid: assume this to be true.
+*/
+
+
+class MagicSquares{
+
+	public static void main(String [ ] args){
+ 
+
+	}
+}
