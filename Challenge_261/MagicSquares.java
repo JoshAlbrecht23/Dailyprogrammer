15f972b818cf04b461aff844cabf99d552034ac9
Josh Albrecht <joshmalbrecht@gmail.com>
2016-04-06 14:50:32 -0500

    Added some code

--- Challenge_261/MagicSquares.java
+++ Challenge_261/MagicSquares.java
@@ -15,8 +15,71 @@ in the grid: assume this to be true.
 
 class MagicSquares{
 
+	boolean calculate ( int[] array ){
+
+		System.out.print("[") ; 
+		for( int i = 0; i<9 ;i++){
+			System.out.print(array[i] + " ") ;
+		}
+		System.out.print("]\n") ; 
+
+
+		//Row check
+		for ( int i = 0 ; i < 9 ; i+=3 ){
+			
+			int sum = 0 ;
+
+			for( int j = i ; j <= i + 3 ; j++ ){
+
+				if ( j = i + 3){
+					if ( sum != 15)
+						return false ; 
+				}else{
+					sum += array[j] ;
+				}
+
+			}
+		}
+
+		//Column check
+		for ( int i = 0; i < 3 ; i++){
+
+			int sum = 0 ;
+
+			for( int j = i + 3; j <= i + 6 ; j+= 3){
+
+				if ( j == i + 6){
+					if ( sum != 15)
+						return false ; 
+				}else{
+					sum += array[i] ;
+				}	
+			}
+		}
+
+		//Diagonal check
+
+
+
+	}
+
+
 	public static void main(String [ ] args){
  
+		MagicSquares square = new MagicSquares() ; 
+
+		//Test inputs.
+		int[] input1 = {8, 1, 6, 3, 5, 7, 4, 9, 2} ; 
+		int[] input2 = {2, 7, 6, 9, 5, 1, 4, 3, 8} ;
+		int[] input3 = {3, 5, 7, 8, 1, 6, 4, 9, 2} ;
+		int[] input4 = {8, 1, 6, 7, 5, 3, 4, 9, 2} ; 
+
+		//Run algorithm
+		System.out.println("> " + square.calculate(input1)) ;
+		System.out.println("> " + square.calculate(input2)) ;
+		System.out.println("> " + square.calculate(input3)) ;
+		System.out.println("> " + square.calculate(input4)) ;
+
 
 	}
 }
