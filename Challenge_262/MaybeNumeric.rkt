788153ca5ecc9a2d50aa34226b33b48c62d7e824
Josh Albrecht <joshmalbrecht@gmail.com>
2016-04-12 14:29:34 -0500

    Challenge 262 Init Commit

---
+++ Challenge_262/MaybeNumeric.rkt
@@ -0,0 +1,18 @@
+#lang plai
+
+;;MaybeNumeric is a function that returns either a number or a string depending on whether the input (string) is a valid description of a number.
+;;sample input (string)
+;;
+;;  123
+;;  44.234
+;;  0x123N
+;;sample output (any)
+;;
+;;  123 (number)
+;;  44.234 (number)
+;;  0x123N (string)
+
+(define MaybeNumeric
+  (lambda (String s)
+    ( cond ( empty? (read-line s)())
+           (                    
